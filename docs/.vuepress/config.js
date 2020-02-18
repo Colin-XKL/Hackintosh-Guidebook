c09b81a93fb2e0f0ee1c1dd3ea506d05e2b3b740
Colin-XKL <Colin_XKL@outlook.com>
2020-02-18 14:18:52 +0800

    update index

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,32 @@
 module.exports = {
   title: 'Hackintosh Guidebook',
-  description: 'Easy way to hackintosh'
+  description: 'Easy way to hackintosh',
+  themeConfig: {
+    lastUpdated: 'Last Updated', // string | boolean
+    sidebar:[
+      {
+        title:"基础篇",
+        path:'/basic/',
+        collapsable:true,
+        sidebarDepth:2,
+        children:[
+          '/basic',
+          'common-tools',
+          'quick-setup',
+          'shopping-guide',
+          'about-kext'
+        ]
+      },
+      {
+        title:"进阶篇",
+        path:'/senior/',
+        collapsable:true,
+        sidebarDepth:2,
+        children:[
+          '/senior',
+
+        ]
+      }
+    ]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -5,6 +5,8 @@
 ## Description
 本项目旨在为有一定基础的黑苹果新人（至少懂分区和重装系统），提供一些指引，让更多有兴趣且有能力的朋友吃上黑苹果
 
+> 手册内容正在编写中，欢迎关注过参与编写！
+
 ## Contribute
 如果本手册中有部分明显词句错误/术语描述不准确的地方，欢迎到Github提出issue/直接提交修改   
 Github项目地址：[https://github.com/Colin-XKL/Hackintosh-Guidebook](https://github.com/Colin-XKL/Hackintosh-Guidebook)  
